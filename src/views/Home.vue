5901b5374738575b387c884a9993f73cd07598ee
soskamichal <michalkososka@gmail.com>
2022-03-07 01:35:52 +0100

    tailwind added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,37 @@
 <template>
   <div class="home">
-    <h1>Testing, attention please</h1>
-    <p>feel the tension as soon as someone mentions me</p>
+    <div class="my-5">
+      <h1 class="text-lg">Top posts from hackernews!</h1>
+    </div>
+    <div class="flex-1 flex-wrap">
+      <div class="w-full md:w-1/2 lg:w-1/3 border border-round-1 border-slate-2">
+        <h3>A post on hackernews</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, itaque quam. Illo iste architecto inventore sint earum tenetur neque minus ad deserunt, eum dolore, voluptates, odit facilis adipisci tempora. Quisquam!</p>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/3 border border-round-1 border-slate-2">
+        <h3>A post on hackernews</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, itaque quam. Illo iste architecto inventore sint earum tenetur neque minus ad deserunt, eum dolore, voluptates, odit facilis adipisci tempora. Quisquam!</p>
+      </div>
+      <div class="w-full md:w-1/2 lg:w-1/3 border border-round-1 border-slate-2">
+        <h3>A post on hackernews</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Impedit, itaque quam. Illo iste architecto inventore sint earum tenetur neque minus ad deserunt, eum dolore, voluptates, odit facilis adipisci tempora. Quisquam!</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex'
+import { mapActions } from "vuex";
 
 export default {
-  name: 'Home',
+  name: "Home",
 
   async created() {
-    await this.getMostPopular()
-    console.log(this.$store.state.mostPopular)
+    await this.getMostPopular();
   },
 
   methods: {
-    ...mapActions(['getMostPopular']),
-
-  }
-}
+    ...mapActions(["getMostPopular"]),
+  },
+};
 </script>
